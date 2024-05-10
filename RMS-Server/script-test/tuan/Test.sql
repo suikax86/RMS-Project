35cc091f1315886d864e2ccb2627b18f056906d8
Tuan Nguyen <minhtuan034015@gmail.com>
2024-05-10 19:16:27 +0700

    script proc

---
+++ RMS-Server/script-test/tuan/Test.sql
@@ -0,0 +1,20 @@
+use RMS
+exec p_getCVList 'Dang xu ly'
+exec p_getCVList NULL
+
+exec p_process_cv 1
+exec p_process_cv 9
+
+exec p_getcvlist_company 2
+exec p_getcvlist_company 4
+
+exec p_update_status 1, 'Da phan hoi', 'asdasd'
+exec p_update_status 9, 'Da phan hoi', 'asdasd'
+
+exec p_getcv_description 1, 0
+exec p_getcv_description 1, 1
+exec p_getcv_description 9, 1
+
+exec p_getapproved_company 1
+exec p_getapproved_company 9
+
