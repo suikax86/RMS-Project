65c1e509c158b23dd6b0b149b0b92c784f560f36
suikax86 <suikax86@outlook.com>
2024-05-10 12:17:13 +0700

    something

---
+++ RMS-Server/Program.cs
@@ -0,0 +1,36 @@
+using System.Data;
+using System.Data.SqlClient;
+using RMS_Server.Interfaces;
+using RMS_Server.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// inject db connection
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string is null");
+builder.Services.AddTransient<IDbConnection>(_ => new SqlConnection(connectionString));
+// inject services
+builder.Services.AddTransient<IStoredProcedureService, StoredProcedureService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
