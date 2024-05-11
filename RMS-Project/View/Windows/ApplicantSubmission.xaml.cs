64a9aadbce2dbf16a8f9bbc130f0035c8a03e65a
suikax86 <suikax86@outlook.com>
2024-05-11 19:08:18 +0700

    update something

---
+++ RMS-Project/View/Windows/ApplicantSubmission.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace RMS_Project.View.Windows;
+
+public partial class ApplicantSubmission : Window
+{
+    public ApplicantSubmission()
+    {
+        InitializeComponent();
+    }
+}
