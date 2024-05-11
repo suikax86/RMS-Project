92567b19ad09996d26a2a0e9e0cf832e4376ca2e
Tuan Nguyen <minhtuan034015@gmail.com>
2024-05-11 20:57:05 +0700

    remove unused UI

--- RMS-Project/View/CVReviewMain.xaml.cs
+++ RMS-Project/View/CVReviewMain.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Navigation;
+using RMS_Project.View;
 
 namespace PTTK_DuyetHoSo;
 
@@ -9,32 +12,48 @@ namespace PTTK_DuyetHoSo;
 /// </summary>
 public partial class CVReviewMain : Window
 {
-    private readonly CVList _cvList;
 
-    private readonly CVProcessingList _processingList = new();
+    public ObservableCollection<CV> cvList { get; set; }
 
     public CVReviewMain()
     {
         InitializeComponent();
-        _processingList = new CVProcessingList();
-        _cvList = new CVList();
-        contentFrame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+        DataContext = this;
+        // Mock data
+        cvList = new ObservableCollection<CV>
+        {
+            new() { CV_ID = "CV1", Position = "Manager", Company = "Company A", Status = "unprocessed" },
+            new() { CV_ID = "CV2", Position = "Software Engineer", Company = "Company A", Status = "unprocessed" },
+            new() { CV_ID = "CV3", Position = "Data Analyst", Company = "Company B", Status = "unprocessed" },
+            new() { CV_ID = "CV4", Position = "Product Manager", Company = "Company C", Status = "unprocessed" },
+            new() { CV_ID = "CV5", Position = "UX Designer", Company = "Company D", Status = "unprocessed" }
+        };
     }
 
-    private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    private void ListView_MouseDoubleClick(object sender, MouseEventArgs e)
     {
-        var tabControl = sender as TabControl;
-        var selectedIndex = tabControl.SelectedIndex;
-
-        switch (selectedIndex)
+        var selectedItems = CVList_ListView.SelectedItems;
+        if (selectedItems.Count > 0)
         {
-            case 0:
-                contentFrame.Content = _cvList;
-                break;
-
-            case 1:
-                contentFrame.Content = _processingList;
-                break;
+            var selectedItem = selectedItems[0] as CV;
+            if (selectedItem != null)
+            {
+                var cvId = selectedItem.CV_ID;
+                var detailsWindow = new CVDetail_Emp(cvId);
+                detailsWindow.ShowDialog();
+            }
         }
     }
+
+
+}
+
+
+public class CV
+{
+    public string CV_ID { get; set; }
+    public string Position { get; set; }
+    public string Company { get; set; }
+    public string Status { get; set; }
+    public bool IsChecked { get; set; }
 }
